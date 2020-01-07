c3a8ba4550ba0a95099638c4c7adf1301bf47b87
Shreyas Rane <55063881+twopiaresquared@users.noreply.github.com>
2020-01-07 13:09:43 -0500

    initial code

---
+++ main.java
@@ -0,0 +1,31 @@
+
+import java.io.*; 
+  
+class GFG  
+{ 
+    static void printCollatz(int n) 
+    { 
+       
+        while (n != 1) 
+        { 
+            System.out.print(n + " "); 
+      
+            // If n is odd 
+            if ((n & 1) == 1) 
+                n = 3 * n + 1; 
+      
+            // If even 
+            else
+                n = n / 2; 
+        } 
+      
+        // Print 1 at the end 
+        System.out.print(n); 
+    } 
+      
+    // Driver code 
+    public static void main (String[] args)  
+    { 
+        printCollatz(6); 
+    } 
+} 
